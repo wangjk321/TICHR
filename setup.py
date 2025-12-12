273b62cb75243a28f38c197948321baa3c4366e0
wangjk321 <wangjk321@gmail.com>
2025-12-12 20:30:15 +0800

    check stable

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tichr',
-    version='0.1.5',
+    version='0.1.7',
     author='Jiankang Wang',
     author_email='wangjk321@gmail.com',
     description='TICHR: A computational tool designed to investigate transcriptional regulation',
